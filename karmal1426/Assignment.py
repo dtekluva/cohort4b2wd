6c4158131060cce1ec78284ff4ab69c65e41c2a9
alogba omotayo <alogbaomotayo1426@gmail.com>
2021-04-26 19:43:44 +0800

    commit

---
+++ karmal1426/Assignment.py
@@ -0,0 +1,6 @@
+My First week in univelcity
+My name is Omotayo Alogba, A new student of Univelcity. I would like to use this medium to write write my first week experience in this institution.
+My first week as a student of univelcity as opened my eyes to the besic knewledge of becoming a programmer, By introducing softwares, such as, GITHUB, VSCODE, GITBASH, and its basic command command prompts, to develop myself as a programmer
+Still speaking on my first week in univelcity, I've also met new people, (course mates and potential work buddies).
+I am really glad to have this opportunity to not only meet new people, But to develop myself on coding as a whole, which i love and plan on building a career on.
+In all, I am glad of how my first week in as a stutent of this institute went, And also looking forward to becoming who i want to be, A computer programmer
