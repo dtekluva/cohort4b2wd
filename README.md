c72a4de3864ff9e207d26c4ba62f77a4f5da2643
dtekluva <kboysreel@gmail.com>
2021-04-26 13:52:40 +0100

    UDATE

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Code for cohort4b2wd 
 https://opensource.com/article/18/1/step-step-guide-git
 Think like a programmer - https://www.freecodecamp.org/news/how-to-think-like-a-programmer-lessons-in-problem-solving-d1d8bf1de7d2/
+PYTHON BASICS: https://www.pythontutorial.net/
---
+++ atha/introduction/syntax_n_semantics.py
@@ -0,0 +1,23 @@
+# for i in range(30) # SYNTAX ERROR
+    # print(i)
+
+# if
+# x = 30
+
+# = 30 boy
+
+# ABOVE ARE SYNTAX ERRORS
+
+# SEMANTIC ERRORS
+
+# print(ade)# name error
+
+# ade = 30
+
+# ade - kunle # name error
+
+# 23/0 #ZeroDivisionError:
+
+# "three" - 23 # Type error
+# Print("ade")
+
