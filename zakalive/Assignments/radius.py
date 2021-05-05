f81ad7bed7f7bc8ff40ea7269b6a2f7acce00e49
abayomiDare <abayomi5991@gmail.com>
2021-05-05 09:55:19 +0100

    deleted

--- zakalive/Assignments/radius.py
+++
@@ -1,6 +0,0 @@
-from math import pi
-
-radius = int(input()) 
-
-area = pi*(radius**2)
-print('area =', area)
--- zakalive/Assignments/time.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-time_now = datetime.datetime.now()
-day = time_now.day
-month = time_now.month
-
-new_format_time = time_now.strftime("%A - %b,%d,%Y, (%I%p)")
-print(new_format_time)
