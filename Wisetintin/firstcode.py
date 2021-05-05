4d5b2e08cacb65f5b474114d7ebafbbaf5a93bc1
wisetintin <olaifasikelola@gmail.com>
2021-05-05 15:35:17 +0100

    add

--- Wisetintin/firstcode.py
+++ Wisetintin/firstcode.py
@@ -1,1 +1,6 @@
-print('hello world')
+# print('hello world')
+
+with open(r"C:\Users\USER\Desktop\data science\cohort4b2wd\Wisetintin\Names", "r") as names:
+    print(names.read())
+
+print(names.read())
