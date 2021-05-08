4c9587c8ed55b0e62f1a0c62e611a362ec2e346d
mahoneslim07 <titus.chukwuemeka07@gmail.com>
2021-05-08 08:27:28 +0100

    completed

--- Mahoneslim07/Study.py
+++ Mahoneslim07/Study.py
@@ -1,10 +1,42 @@
-# counter = 0
-# def update_counter():
-#     global counter
-#     counter = counter + 1
+# #Calculating Total Amopunt Payable, Simple Interest
 
-# update_counter()
-# counter
+# def simple_interest(P, R, T):
+#     SI = (P * R * T)/100
+#     return SI
 
-y = "emeka"
-assert y == "tobe", "y should be 'emeka'"
+# P = int(input("Enter Principal amount : "))
+# R = int(input("Enter the rate on investment : "))
+# T = int(input("Enter the duration of investment : "))
+
+# SI = simple_interest(P, R, T)
+
+# print("Simple interest : {}". format(SI))
+
+# def total_amount_payable(SI):
+#     Profit = SI + P
+#     return Profit
+# Profit = total_amount_payable(SI)
+
+# print("Total Amount Payable :{}". format(Profit))
+
+
+#Formaular to aid the distribution of Sweets amongst Friends
+
+def love_sharing_formular(A, B, C):
+    LSF = (A + B + C)//3
+    return LSF
+A = int(input("Enter value for the first sweet brought in : "))
+B = int(input("Enter value for the Second sweet brought in : "))
+C = int(input("Enter value for the third Sweet brought in : "))
+
+LSF = love_sharing_formular(A, B, C)
+
+def crushed_value(A, B, C):
+    Crushed = (A + B + C)%3
+    return Crushed
+Crushed = crushed_value(A, B, C)
+
+
+print(f'"Hello guys, Each of you should get {LSF} sweets and {Crushed} should be crushed')
+
+    
--- atha/introduction/syntax_n_semantics.py
+++ atha/introduction/syntax_n_semantics.py
@@ -1,23 +1,2 @@
-# for i in range(30) # SYNTAX ERROR
-    # print(i)
-
-# if
-# x = 30
-
-# = 30 boy
-
-# ABOVE ARE SYNTAX ERRORS
-
-# SEMANTIC ERRORS
-
-# print(ade)# name error
-
-# ade = 30
-
-# ade - kunle # name error
-
-# 23/0 #ZeroDivisionError:
-
-# "three" - 23 # Type error
-# Print("ade")
-
+p = (2 + 10 + 4)%3
+print(p)
