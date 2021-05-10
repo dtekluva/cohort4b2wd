6a9c10e5388793be5fa2fa7a08e7bab9af7b0415
abayomiDare <abayomi5991@gmail.com>
2021-05-10 09:40:41 +0100

    new changes

--- abayomiDare/assignment002.py
+++
@@ -1,4 +0,0 @@
-# i ran my code directly in my terminal 
-# like py --version
-# PS C:\Users\abayomiDare\Desktop\Univelcity\cohort4b2wd> py --version
-# Python 3.7.8
---
+++ abayomiDare/week3assignment.py
@@ -0,0 +1,14 @@
+# assignment 1
+
+
+user_first_input = int(input('Enter dividend:\n'))
+user_second_input = int(input('Enter divisor:\n'))
+soln_without_remainder = user_first_input // user_second_input
+remainder = user_first_input % user_second_input
+
+print(f'{soln_without_remainder} remainder {remainder}')
+
+
+
+
+
