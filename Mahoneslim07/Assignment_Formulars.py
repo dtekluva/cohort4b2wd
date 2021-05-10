bd28746d497e1e76b4b996197265a92f64e0f35d
mahoneslim07 <titus.chukwuemeka07@gmail.com>
2021-05-10 02:02:25 +0100

    updated

--- Mahoneslim07/Assignment_Formulars.py
+++ Mahoneslim07/Assignment_Formulars.py
@@ -39,7 +39,7 @@ print("Total Amount Payable :{}". format(Profit))
 # #Formaular to aid the distribution of Sweets amongst Friends
 
 def love_sharing_formular(A, B, C):
-    LSF = (A + B + C)//3
+    LSF = int((A + B + C)//3)
     return LSF
 A = float(input("Enter value for the first sweet brought in : "))
 B = float(input("Enter value for the Second sweet brought in : "))
@@ -48,10 +48,10 @@ C = float(input("Enter value for the third Sweet brought in : "))
 LSF = love_sharing_formular(A, B, C)
 
 def crushed_value(A, B, C):
-    Crushed = (A + B + C)%3
+    Crushed = int((A + B + C)%3)
     return Crushed
 Crushed = crushed_value(A, B, C)
 
-print(f'Hello guys, Each of you should get {LSF} sweets and {Crushed} should be crushed')
+print(f'Hello guys, Each of you should get {LSF} sweets and {Crushed if Crushed > 0 else "None"} should be crushed')
 
     
--- Mahoneslim07/classExmaples.py
+++ Mahoneslim07/classExmaples.py
@@ -1,5 +1,16 @@
-x = int(input("Enter Dividen : "))
-y = int(input("Enter Divisor : "))
 
-def division_progm(x%y):
-    print(division_progm)
+def love_sharing_formular(A, B, C):
+    LSF = int((A + B + C)//3)
+    return LSF
+A = float(input("Enter value for the first sweet brought in : "))
+B = float(input("Enter value for the Second sweet brought in : "))
+C = float(input("Enter value for the third Sweet brought in : "))
+
+LSF = love_sharing_formular(A, B, C)
+
+def crushed_value(A, B, C):
+    Crushed = int((A + B + C)%3)
+    return Crushed
+Crushed = crushed_value(A, B, C)
+
+print(f'Hello guys, Each of you should get {LSF} sweets and {Crushed if Crushed > 0 else "None"} should be crushed')
