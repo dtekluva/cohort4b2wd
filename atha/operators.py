7d33bcea58551fed41ed3047e95aff62d88e84bc
dtekluva <kboysreel@gmail.com>
2021-05-10 13:10:59 +0100

    update

---
+++ atha/operators.py
@@ -0,0 +1,34 @@
+# identity operator
+
+# x = 20 
+# y = 30
+# z = 20
+# q = int(40/2)
+
+# print(x is y)
+# print(x is z)
+# print(q is x)
+
+# print(q, type(q), x, type(x))
+
+#membership operator
+
+# name = "Mr Sanjay Miyake"
+# name2 = "Mrs Sanjay Miyake"
+
+# print("Mr" in name)
+# print("Mr" in name2)
+# print("Mrs" in name2)
+# print("Mrs" in name)
+# print("mrs" in name)
+
+# scores = [23, 40, 11, 55, 90]
+
+# print(90 in scores)
+
+# text = input("Please enter text : ")
+
+# char = "Bola"
+# x = char in text
+
+# print(f"{text}. contains '{char}' : {x}")
