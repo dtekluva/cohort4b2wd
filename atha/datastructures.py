abbff9b10b1749a0ca10d203afb016b8a6188b5d
dtekluva <kboysreel@gmail.com>
2021-05-26 12:56:06 +0100

    update

--- atha/datastructures.py
+++ atha/datastructures.py
@@ -13,10 +13,27 @@
 #     print(func("adaora"))
 
 
-names = ["ade", "kunle", "john", "sam"]
+# names = ["ade", "kunle", "john", "sam"]
 
-print(names)
+# print(names)
 
-names[1] = "sam"
+# names[1] = "sam"
 
-print(names)
+# print(names)
+
+file_path = r"C:\Users\kboys\OneDrive\Desktop\CLASSES\UNIVELCITY CLASSES\cohort4b2wd\materials\gtb_doc.csv"
+file = open(file_path, "r")
+
+vat_deb = []
+
+for line in file.readlines():
+
+    split_line = line.split(",")
+    remarks = split_line[-1]
+    debits = split_line[3]
+
+    if "VALUE ADDED TAX" in remarks:
+        vat_deb.append(debits)
+        print(remarks)
+
+print(vat_deb)
