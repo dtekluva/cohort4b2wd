f81d2e309531941f999b7d8a1c4206627133f608
dtekluva <kboysreel@gmail.com>
2021-06-02 16:03:42 +0100

    update

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Learn more about datetime - https://www.programiz.com/python-programming/datetim
 variables - https://realpython.com/python-variables/
 datatypes - https://realpython.com/python-data-types/
 functions - https://realpython.com/defining-your-own-python-function/
+
+dicts - https://realpython.com/python-dicts/
+list - https://realpython.com/python-lists-tuples/
--- atha/oop.py
+++ atha/oop.py
@@ -54,34 +54,84 @@
 # print(kunle.salary)
 
 
-class Employee: # BASE EMPLOYEE CLASS
+# class Employee: # BASE EMPLOYEE CLASS
 
-    #### CLASS ATTRIBUTES ###
-    hmo_id = 132242
-    account_type = "current salary account"
-    access_code = 4563
+#     #### CLASS ATTRIBUTES ###
+#     hmo_id = 132242
+#     account_type = "current salary account"
+#     access_code = 4563
 
-    #### INSTANCE ATTRIBUTES ###
-    def __init__(self, name, staff_id, salary, dept, company_name):
-        self.name = name
-        self.staff_id = staff_id
-        self.salary = salary
-        self.dept = dept
-        self.company_name = company_name
+#     #### INSTANCE ATTRIBUTES ###
+#     def __init__(self, name, staff_id, salary, dept, company_name):
+#         self.name = name
+#         self.staff_id = staff_id
+#         self.salary = salary
+#         self.dept = dept
+#         self.company_name = company_name
 
-    def describe(self):
-        print(f"""
-                Hello my name is {self.name},
-                and I work for {self.company_name},
-                With a salary of {self.salary}.
+#     def describe(self):
+#         print(f"""
+#                 Hello my name is {self.name},
+#                 and I work for {self.company_name},
+#                 With a salary of {self.salary}.
 
-                You can check me out, my staff id is {self.staff_id}
-                """)
+#                 You can check me out, my staff id is {self.staff_id}
+#                 """)
+
+#     def __str__(self):
+#         return self.name
+
+# ade = Employee("Ade Bami", 12110, 230000, "Administration", "Toptal")
+# kunle = Employee("Samuel Bami", 21009, 100000, "Technical", "KPMG")
+
+# print(ade.describe())
+# print(ade.commission)
+
+# print(kunle.describe())
+# print(str(kunle))
+
+# class Contract_Employee(Employee):
+#     commission = 0.1
+
+#     def describe(self):
+#         super().describe()
+#         print(f"Also i pay a commision of {self.commission}.")
+
+# shane = Contract_Employee("shane", 1923, 200000, "Admin", "Andela")
+# shane.describe()
+# print(shane.commission)
+
+folder = r"C:\Users\kboys\OneDrive\Desktop\CLASSES\UNIVELCITY CLASSES\cohort4b2wd\materials"
+
+def write_to_file( username, password):
+
+    file_name = "\logins.csv"
+    file = open(folder + file_name, "a")
+    file.write(f"{username},,{password}\n")
+
+    file.close()
+
+def login( username, password):
+
+    file_name = "\logins.csv"
+    file = open(folder + file_name, "r")
+    data = file.readlines()
+
+    for line in data:
+        # print(line)
+        splitted_line = line.split(",,")
+        stored_username, stored_password = splitted_line
+
+        if username == stored_username:
+            print("Corect username")
+            # print(password , stored_password, password == stored_password)
+            if password == stored_password.replace("\n", ""):
+                print("correct password")
+                print("Successfully logged in..!!!")
+        else:
+            print("Incorrect details")
 
 
-ade = Employee("Ade Bami", 12110, 230000, "Administration", "Toptal")
-kunle = Employee("Samuel Bami", 21009, 100000, "Technical", "KPMG")
 
-print(ade.describe())
-
-print(kunle.describe())
+# write_to_file("ayo", "278143462")
+login("atha", "123456")
