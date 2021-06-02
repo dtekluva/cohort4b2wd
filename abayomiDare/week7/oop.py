0e07ace4de7dba88e231d1026910009be16a842b
abayomiDare <abayomi5991@gmail.com>
2021-06-02 14:38:33 +0100

    update

--- abayomiDare/week7/oop.py
+++ abayomiDare/week7/oop.py
@@ -1,19 +1,34 @@
 from playsound import playsound
 class Animal: 
-
     # INSTANCE ATTRIBUTE
-    def __init__(self, name, type, furColor):
+    base_dir = "/home/abayomidare/Desktop/Univelcity/cohort4b2wd/abayomiDare/"
+    
+    def __init__(self, name, type, furColor, sound):
         self.name = name
         self.type = type
         self.furColor = furColor
+        self.sound = sound
+
     def describe(self):
-        print(f"""
+        print(f"""dog
         I am {self.name}
         with a furcolor {self.furColor}
         belong to {self.type}
         """)
-        playsound('cat_Meow.mp3')
+        self.make_sound()
 
-cat = Animal('Smudge','Snowshoe','Tuxedo')
+    def make_sound(self):
+
+        playsound(f"{self.base_dir}{self.sound}")
+
+
+        
+
+
+
+cat = Animal('Smudge','Snowshoe','Tuxedo', "cat_Meow.mp3")
 cat.describe()
+
+dog = Animal('Billy','Terrier Mixed Breed', 'Brown',  "Terrier Mixed Breed Snarl .mp3")
+dog.describe()
 
