c1570a1799bf386eb1f071635fdb0928167dd538
dtekluva <kboysreel@gmail.com>
2021-06-02 14:40:12 +0100

    update

--- atha/oop.py
+++ atha/oop.py
@@ -27,6 +27,33 @@
 # print(kunle.account_type)
 # print(kunle.__dir__())
 
+# class Employee: # BASE EMPLOYEE CLASS
+
+#     #### CLASS ATTRIBUTES ###
+#     hmo_id = 132242
+#     account_type = "current salary account"
+#     access_code = 4563
+
+#     #### INSTANCE ATTRIBUTES ###
+#     def __init__(self, name, staff_id, salary, dept):
+#         self.name = name
+#         self.staff_id = staff_id
+#         self.salary = salary
+#         self.dept = dept
+
+
+# ade = Employee("Ade Bami", 12110, 230000, "Administration")
+# kunle = Employee("Samuel Bami", 21009, 100000, "Technical")
+
+# print(ade.hmo_id)
+# print(ade.account_type)
+# print(ade.salary)
+
+# print(kunle.hmo_id)
+# print(kunle.account_type)
+# print(kunle.salary)
+
+
 class Employee: # BASE EMPLOYEE CLASS
 
     #### CLASS ATTRIBUTES ###
@@ -35,20 +62,26 @@ class Employee: # BASE EMPLOYEE CLASS
     access_code = 4563
 
     #### INSTANCE ATTRIBUTES ###
-    def __init__(self, name, staff_id, salary, dept):
+    def __init__(self, name, staff_id, salary, dept, company_name):
         self.name = name
         self.staff_id = staff_id
         self.salary = salary
         self.dept = dept
+        self.company_name = company_name
+
+    def describe(self):
+        print(f"""
+                Hello my name is {self.name},
+                and I work for {self.company_name},
+                With a salary of {self.salary}.
+
+                You can check me out, my staff id is {self.staff_id}
+                """)
 
 
-ade = Employee("Ade Bami", 12110, 230000, "Administration")
-kunle = Employee("Samuel Bami", 21009, 100000, "Technical")
+ade = Employee("Ade Bami", 12110, 230000, "Administration", "Toptal")
+kunle = Employee("Samuel Bami", 21009, 100000, "Technical", "KPMG")
 
-print(ade.hmo_id)
-print(ade.account_type)
-print(ade.salary)
+print(ade.describe())
 
-print(kunle.hmo_id)
-print(kunle.account_type)
-print(kunle.salary)
+print(kunle.describe())
